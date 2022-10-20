50cc28563c3d6e1223e8625f056645736a6a8dcc
YuXiang Dong <63429467+YuXiang-Dong@users.noreply.github.com>
2022-10-20 15:06:41 -0400

    Add files via upload

---
+++ PersonAlreadyExistsException.java
@@ -0,0 +1,9 @@
+/**
+ * only evoked if the person already exists in the array
+ * @author Yu Xiang Dong
+ */
+public class PersonAlreadyExistsException extends Exception{
+    public PersonAlreadyExistsException() {
+        super();
+    }
+}
---
+++ PersonDoesNotExistsException.java
@@ -0,0 +1,9 @@
+/**
+ * only evoked if the person does not exist in the array
+ * @author Yu Xiang Dong
+ */
+public class PersonDoesNotExistsException extends Exception{
+    public PersonDoesNotExistsException(){
+        super();
+    }
+}
